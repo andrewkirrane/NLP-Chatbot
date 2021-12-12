26de58b7bb2b35ff2800bc65f6993958ed834f6a
andrewkirrane <93559476+andrewkirrane@users.noreply.github.com>
2021-12-11 22:56:40 -0800

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,19 @@
+# Import modules and libraries
 import random
 import json
 import torch
 from model import NeuralNetwork
 from nltk_util import tokenize, bag_of_words
 
+# Open data file, save data to dictionary
 with open('data.json', 'r') as f:
     data = json.load(f)
 
+# Open and load trained model
 FILE = "net.pth"
 net = torch.load(FILE)
 
+# Save trained model parameters
 input_size = net["input_size"]
 hidden_size = net["hidden_size"]
 output_size = net["output_size"]
@@ -17,33 +21,41 @@ all_words = net["all_words"]
 tags = net["tags"]
 model_state = net["model_state"]
 
+# Initialize new model based on trained model
 model = NeuralNetwork(input_size, hidden_size, output_size)
 model.load_state_dict(model_state)
 model.eval()
 
-chatbot_name = "Charlie"
+# User input to name chatbot
+chatbot_name = input("What would you like to name the chatbot?\n")
+
+# Loop for conversation with chatbot
 print("Let's chat! (type 'quit' to exit)")
 while True:
-    sentence = input('You: ')
+    sentence = input('You: ') # User input for next line in conversation
+    # Escape from conversation
     if sentence == "quit":
         break
 
-    sentence = tokenize(sentence)
-    x = bag_of_words(sentence, all_words)
-    x = x.reshape(1, x.shape[0])
-    x = torch.from_numpy(x)
+    sentence = tokenize(sentence) # Tokenize user input
+    x = bag_of_words(sentence, all_words) # Create a bag of words with user input
+    x = x.reshape(1, x.shape[0]) # Reshape to one row
+    x = torch.from_numpy(x) # Convert numpy array to torch tensor
 
-    output = model(x)
-    _, prediction = torch.max(output, dim = 1)
-    tag = tags[prediction.item()]
+    output = model(x) # Feed user input data through model
+    _, prediction = torch.max(output, dim = 1) # Returns prediction 
+    tag = tags[prediction.item()] # Return predicted tag
 
+    # Calculate probability of output being correct
     probs = torch.softmax(output, dim = 1)
-    prob = probs[0][prediction.item()]
+    prob = probs[0][prediction.item()] 
 
-    if prob.item() > 0.75:
-        for kw in data["data"]:
-            if tag == kw["tag"]:
-                print(f"{chatbot_name}: {random.choice(kw['responses'])}")
+    if prob.item() > 0.75: # Threshold of 75%, else bot does not understand
+        # For tag in data dictionary
+        for keyword in data["data"]:
+            # If tag matches predicted tag
+            if tag == keyword["tag"]:
+                print(f"{chatbot_name}: {random.choice(keyword['responses'])}") # Print a random response from the list of responses
 
     else:
-        print(f"{chatbot_name}: I do not understand...")
+        print(f"{chatbot_name}: I do not understand...") # If under 75% probablility, print "I do not understand..."
